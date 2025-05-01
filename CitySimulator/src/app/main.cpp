9c7534226960b16ecc9e89f2dbd3d542a6cb5c91
Ivan Shcherbina <ivan.shcherbina@yandex.ru>
2025-05-01 15:43:22 +0300

    add qt

--- CitySimulator/src/app/main.cpp
+++ CitySimulator/src/app/main.cpp
@@ -1,13 +1,42 @@
-// /CitySimulator/src/app/main.cpp
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QPushButton>
 
-#include <iostream>
 #include <core/test.h>
 
-int main() {
-    std::cout << "Welcome to TJamSimulator!" << std::endl;
-
-    // Calling a function from the core library (as an example)
+int main(int argc, char *argv[])
+{
     testFunction();
-
-    return 0;
+    // Create the application
+    QApplication app(argc, argv);
+    
+    // Create the main window widget
+    QWidget window;
+    window.setWindowTitle("Basic Qt6 Application");
+    window.resize(400, 300);
+    
+    // Create a layout for our window
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    
+    // Add a label
+    QLabel *label = new QLabel("Hello Qt6 World!");
+    label->setAlignment(Qt::AlignCenter);
+    layout->addWidget(label);
+    
+    // Add a button
+    QPushButton *button = new QPushButton("Click Me");
+    layout->addWidget(button);
+    
+    // Connect the button's clicked signal to a lambda function
+    QObject::connect(button, &QPushButton::clicked, [label]() {
+        label->setText("Button was clicked!");
+    });
+    
+    // Show the window
+    window.show();
+    
+    // Start the event loop
+    return app.exec();
 }
