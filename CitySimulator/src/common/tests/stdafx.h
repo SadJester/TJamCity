bdeb4bddf5c2e5e65cb57c44a49f11fac90b9801
TraurigerNarr <ivan.shcherbina@yandex.ru>
2025-08-21 22:24:59 +0300

    Dev/opt object pool (#33)
    * gperf integration
    * windows building in proper folders
    * benchmarks for object-pool

--- CitySimulator/src/common/tests/stdafx.h
+++ CitySimulator/src/common/tests/stdafx.h
@@ -3,4 +3,13 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <benchmark/benchmark.h>
+
 #include <unordered_set>
+#include <cstdint>
+#include <vector>
+#include <random>
+#include <array>
+#include <string>
+#include <thread>
+#include <algorithm>
